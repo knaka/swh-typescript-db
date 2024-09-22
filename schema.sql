2ad3dd97845b33883a29d7f402f3b705066e5358
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-09-22 13:22:53 +0900

    .

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
   id integer PRIMARY KEY,
   username text NOT NULL,
+  new_column text NOT NULL,
   updated_at datetime NOT NULL DEFAULT current_timestamp,
   created_at datetime NOT NULL DEFAULT current_timestamp
 );
--- task-project.lib.sh
+++ task-project.lib.sh
@@ -2,6 +2,7 @@
 set -o nounset -o errexit
 
 db_path="$(dirname "$0")"/main.db
+schema_path="$(dirname "$0")"/schema.sql
 
 task_db__dump() {
   cd "$(dirname "$0")" || exit 1
@@ -10,5 +11,22 @@ task_db__dump() {
 
 task_db__migrate() {
   cd "$(dirname "$0")" || exit 1
-  cross_exec ./gobin run sqlite3def --file=./schema.sql "${db_path}"
+  cross_exec ./gobin run sqlite3def --file="${schema_path}" "${db_path}"
+}
+
+task_db__diff() {
+  cd "$(dirname "$0")" || exit 1
+  if ! test -f "${db_path}"
+  then
+    echo "-- initial --"
+    cat "${schema_path}"
+    exit 0
+  fi
+  # Copy only the schema from the current database to a temporary database.
+  mkdir -p build
+  sh cmd-sqlite3.sh "$db_path" .dump > build/current_schema.sql
+  rm -f build/current.db
+  sh cmd-sqlite3.sh build/current.db < build/current_schema.sql
+  # Then, compare the schema.
+  cross_exec ./gobin run sqlite3def --file="${schema_path}" build/current.db --dry-run
 }
